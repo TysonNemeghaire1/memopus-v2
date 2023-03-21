f162e16f9a3514dfac610f927d85568eb1e456e6
Tyson Nemeghaire <nemeghaire.tyson@outlook.be>
2023-03-21 16:57:21 +0100

    Début du sidemenu

--- src/components/SideMenu/SideMenu.tsx
+++ src/components/SideMenu/SideMenu.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 function SideMenu() {
     return (
-        <div></div>
+        <div>
+            <div className="flex justify-between items-center p-6">
+                <h1 className="text-3xl text-blue-800 font-extrabold pr-6">Memopus</h1>
+                <button className="lg:hidden">X</button>
+            </div>
+        </div>
     );
 }
 
