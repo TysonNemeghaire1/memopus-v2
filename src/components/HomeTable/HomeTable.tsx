9e62edf26f0d5386bdfa1687572dd6738875cfa7
Tyson Nemeghaire <nemeghaire.tyson@outlook.be>
2023-03-22 17:18:46 +0100

    AModifications visuelles

--- src/components/HomeTable/HomeTable.tsx
+++ src/components/HomeTable/HomeTable.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import Coopernet from "../../services/Coopernet";
 import NumberOfTermInColumnInterface from "../../interfaces/NumberOfTermInColumn";
 import columnIndexType from "../../interfaces/columnIndex";
+import {AiOutlineArrowDown} from "react-icons/ai";
 
 function HomeTable() {
 
@@ -45,26 +46,42 @@ function HomeTable() {
     return (
         <>
             <div className="mx-auto h-full w-full lg:w-11/12">
-                <h1 className="p-6 text-xl lg:text-2xl font-extrabold text-blue-800">Mes tableaux</h1>
-                <table className="w-full overflow-hidden lg:rounded-3xl bg-white text-center text-blue-800 text-sm shadow-2xl">
+                <h1 className="py-6 text-xl font-extrabold text-blue-800 lg:text-2xl">Accueil</h1>
+                <table
+                    className="w-full overflow-hidden bg-white text-center text-sm text-blue-800 shadow-2xl lg:rounded-3xl">
                     <thead className="border-b-2 border-b-blue-800">
                     <tr>
-                        <th className="border-b-2 border-b-blue-100 bg-blue-800 py-4 text-white" scope="col"
-                            role={"button"} onClick={() => sortByName()}>{isSmallScreen ? 'Thème' : 'Thématique'}</th>
+                        <th className="flex items-center justify-center border-b-blue-100 bg-blue-800 py-5 text-white border-b-3 gap-1.5"
+                            scope="col"
+                            role={"button"} onClick={() => sortByName()}><span
+                            className="flex items-center justify-center gap-1.5">{isSmallScreen ? 'Thème' : 'Thématique'}
+                            <AiOutlineArrowDown/></span></th>
                         <th scope="col" role="button"
-                            onClick={() => sortByCol(17)}>{isSmallScreen ? 'Apprendre' : 'À apprendre'}</th>
+                            onClick={() => sortByCol(17)}>
+                            <span
+                                className="flex items-center justify-center gap-1">{isSmallScreen ? 'Apprendre' : 'À apprendre'}<AiOutlineArrowDown/></span>
+                        </th>
                         <th scope="col" role="button"
-                            onClick={() => sortByCol(18)}>{isSmallScreen ? 'Peu' : 'Je sais un peu'}</th>
+                            onClick={() => sortByCol(18)}>
+                            <span
+                                className="flex items-center justify-center gap-1">{isSmallScreen ? 'Peu' : 'Je sais un peu'}<AiOutlineArrowDown/></span>
+                        </th>
                         <th scope="col" role="button"
-                            onClick={() => sortByCol(19)}>{isSmallScreen ? 'Bien' : 'Je sais bien'}</th>
+                            onClick={() => sortByCol(19)}>
+                            <span
+                                className="flex items-center justify-center gap-1">{isSmallScreen ? 'Bien' : 'Je sais bien'}<AiOutlineArrowDown/></span>
+                        </th>
                         <th scope="col" role="button"
-                            onClick={() => sortByCol(20)}>{isSmallScreen ? 'Parfaitement' : 'Je sais parfaitement'}</th>
+                            onClick={() => sortByCol(20)}>
+                            <span
+                                className="flex items-center justify-center gap-1">{isSmallScreen ? 'Parfait' : 'Je sais parfaitement'}<AiOutlineArrowDown/></span>
+                        </th>
                     </tr>
                     </thead>
                     <tbody>{numberOfTermInColumn.map((data, index) => (
                         <tr role={'button'} key={data.card_theme_id}
-                            className={`hover:bg-blue-200 border-y border-y-blue-100 semi ${index === numberOfTermInColumn.length - 1 ? "lg:rounded-b-3xl" : ""}`}>
-                            <td className={`py-3 text-white bg-blue-800 px-2 sm:px-0 ${index === numberOfTermInColumn.length - 1 ? "lg:rounded-bl-3xl" : ""}`}>{transformText(data.name)}</td>
+                            className={`hover:bg-blue-200 border-b border-b-blue-100 ${index === numberOfTermInColumn.length - 1 ? "lg:rounded-b-3xl" : ""}`}>
+                            <td className={`font-bold py-3 text-blue-800 bg-blue-50 px-2 sm:px-0 ${index === numberOfTermInColumn.length - 1 ? "lg:rounded-bl-3xl" : ""}`}>{transformText(data.name)}</td>
                             <td>{data.cols['17']}</td>
                             <td>{data.cols['18']}</td>
                             <td>{data.cols['19']}</td>
