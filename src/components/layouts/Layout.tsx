0c3534448b9b89a6abcb32333e91a84c77cfbe8c
Tyson Nemeghaire <nemeghaire.tyson@outlook.be>
2023-03-22 17:22:56 +0100

    Ajout du loader des thématiques

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import {Outlet} from "react-router-dom";
+import {Outlet, redirect, useLoaderData} from "react-router-dom";
 import SideMenu from "../SideMenu/SideMenu";
+import Coopernet from "../../services/Coopernet";
+import ThematicInterface from "../../interfaces/Thematic";
+
+export async function thematicLoader() {
+    try {
+        if (!Coopernet.user.id) return redirect("/login");
+        return {thematics: await Coopernet.getThematics()};
+    } catch (e) {
+        return redirect("/login");
+    }
+}
 
 const Layout = () => {
+    const {thematics}: { thematics: ThematicInterface[] } = useLoaderData() as { thematics: ThematicInterface[] };
     return (
         <>
             <div className="flex h-screen flex-col lg:flex-row">
                 <aside className="h-20 w-full lg:h-full lg:w-1/5">
-                    <SideMenu/>
+                    <SideMenu thematics={thematics}/>
                 </aside>
                 <main className="h-full w-full bg-gray-50">
                     <Outlet/>
