1023c8107d471c1aed44c5d914f517381a18cdf8
Tyson Nemeghaire <nemeghaire.tyson@outlook.be>
2023-03-23 17:07:08 +0100

    Ajout du fetchUser dans le loader

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -2,24 +2,31 @@ import React from 'react';
 import {Outlet, redirect, useLoaderData} from "react-router-dom";
 import SideMenu from "../SideMenu/SideMenu";
 import Coopernet from "../../services/Coopernet";
-import ThematicInterface from "../../interfaces/Thematic";
+import Thematic from "../../interfaces/Thematic";
+import User from "../../interfaces/User";
+
+interface LoaderData {
+    thematics: Thematic[],
+    users: User[]
+}
 
 export async function thematicLoader() {
     try {
         if (!Coopernet.user.id) return redirect("/login");
-        return {thematics: await Coopernet.getThematics()};
+        const [thematics, users] = await Promise.all([Coopernet.getThematics(), Coopernet.getUsers()])
+        return {thematics, users};
     } catch (e) {
         return redirect("/login");
     }
 }
 
 const Layout = () => {
-    const {thematics}: { thematics: ThematicInterface[] } = useLoaderData() as { thematics: ThematicInterface[] };
+    const {thematics, users}: LoaderData = useLoaderData() as LoaderData;
     return (
         <>
             <div className="flex h-screen flex-col lg:flex-row">
                 <aside className="h-20 w-full lg:h-full lg:w-1/5">
-                    <SideMenu thematics={thematics}/>
+                    <SideMenu thematics={thematics} users={users}/>
                 </aside>
                 <main className="h-full w-full bg-gray-50">
                     <Outlet/>
