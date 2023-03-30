1a758d3fc09475df1910b0a0d1557dbf74fe94e0
Tyson Nemeghaire <nemeghaire.tyson@outlook.be>
2023-03-30 16:25:27 +0200

    Décomposition en composant

---
+++ src/components/SideMenu/Nav/Button.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import Thematic from "../../../interfaces/Thematic";
+import List from "./List";
+import { FaChevronDown } from "react-icons/fa";
+import ActionButtonGroup from "./ActionButtonGroup";
+
+interface Props {
+  name: string;
+  id: string;
+  thematicChildren?: Thematic[];
+}
+
+export default function Button({ name, thematicChildren, id }: Props) {
+  const [show, setShow] = useState(false);
+  return (
+    <li
+      className="border-blue-800 p-2.5 hover:border hover:bg-blue-100"
+      onClick={(event) => {
+        event.stopPropagation();
+        setShow(!show);
+      }}
+    >
+      <span className="flex justify-between">
+        {name}
+        <div className="flex items-center gap-0.5">
+          <ActionButtonGroup id={id} />
+          {thematicChildren && (
+            <button
+              className={`text-blue-800 h-fit transition duration-500 p-1 hover:bg-blue-800 hover:text-white rounded-full ${
+                show ? "rotate-180 " : ""
+              }`}
+            >
+              <FaChevronDown />
+            </button>
+          )}
+        </div>
+      </span>
+      {thematicChildren && <List dataArray={thematicChildren} show={show} />}
+    </li>
+  );
+}
