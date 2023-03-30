4eceb09a33d73fa975709078cf1fe37ce62798de
Tyson Nemeghaire <nemeghaire.tyson@outlook.be>
2023-03-30 16:31:44 +0200

    Formatage prettier + ajout Type guard thématique

--- src/interfaces/Thematic.ts
+++ src/interfaces/Thematic.ts
@@ -1,6 +1,12 @@
+import User from "./User";
+
 export default interface Thematic {
-    id: string,
-    pid: number,
-    name: string,
-    children?: Thematic[]
+  id: string;
+  pid: number;
+  name: string;
+  children?: Thematic[];
 }
+
+export const isThematic = (data: Thematic | User): data is Thematic => {
+  return "id" in data;
+};
