3a6234769a4ebb781025de8e5913725d15d28a36
Tyson Nemeghaire <nemeghaire.tyson@outlook.be>
2023-04-11 16:37:44 +0200

    Affichage en grille des données

--- src/components/DataTable/DataTable.tsx
+++ src/components/DataTable/DataTable.tsx
@@ -1,64 +1,69 @@
 import React, { useState } from "react";
-import { redirect, useLoaderData } from "react-router-dom";
-import Thematic from "../../interfaces/Thematic";
-import coopernet from "../../services/Coopernet";
-import Coopernet from "../../services/Coopernet";
-
-export const thematicTableLoader = async () => {
-  try {
-    if (!coopernet.user.id) return redirect("/login");
-    const fetchedThematics = await Coopernet.getThematics();
-    const thematics: Thematic[] = [];
-    for (let fetchedThematic of fetchedThematics) {
-      thematics.push(fetchedThematic);
-      if (fetchedThematic.children) {
-        for (let child of fetchedThematic.children) {
-          thematics.push(child);
-        }
-      }
-    }
-    return { thematics };
-  } catch (e) {
-    return redirect("/login");
-  }
-};
+import { Link, useLoaderData } from "react-router-dom";
+import Thematic, { isThematic } from "../../interfaces/Thematic";
+import User from "../../interfaces/User";
 
 function DataTable() {
-  const { thematics }: { thematics: Thematic[] } = useLoaderData() as {
-    thematics: Thematic[];
-  };
+  const datas = useLoaderData() as Thematic[] | User[];
   const [filter, setFilter] = useState("");
 
   return (
-    <div className="mx-auto h-full w-full space-y-6 lg:w-11/12">
-      <h1 className="pt-6 text-xl font-extrabold text-blue-800 lg:text-2xl">
-        Mes thématiques
-      </h1>
-      <section className="flex flex-col gap-1">
-        <label className="text-blue-800" htmlFor="thematicFilter">
-          Filtre
-        </label>
-        <input
-          type="text"
-          name="thematicFilter"
-          id="thematicFilter"
-          placeholder="Taper votre recherche"
-          className="w-fit rounded-lg ring-1 ring-blue-600 p-1.5"
-          value={filter}
-          onChange={(event) => setFilter(event.target.value)}
-        />
-      </section>
-      <section>
-        {thematics.flatMap((thematic) => {
-          if (
-            filter.length < 2 ||
-            thematic.name.toUpperCase().includes(filter.toUpperCase())
-          ) {
-            return <div>{thematic.name}</div>;
-          }
-          return [];
-        })}
-      </section>
+    <div className="mx-auto h-full w-11/12 sm:mt-8">
+      <div className="bg-white space-y-6 shadow-lg py-5 px-5">
+        <h1 className="pt-6 text-xl font-extrabold text-blue-800 2xl:text-2xl">
+          {isThematic(datas[0]) ? "Mes thématiques" : "Les autres utilisateurs"}
+        </h1>
+        <section className="flex flex-col gap-1">
+          <label className="text-blue-800" htmlFor="thematicFilter">
+            Filtre
+          </label>
+          <input
+            type="text"
+            name="thematicFilter"
+            id="thematicFilter"
+            placeholder="Taper votre recherche"
+            className="w-fit rounded-lg ring-1 ring-blue-600 p-1.5"
+            value={filter}
+            onChange={(event) => setFilter(event.target.value)}
+          />
+        </section>
+        <section className="flex flex-wrap gap-3 w-full">
+          <ul className="grid grid-cols-1 gap-1 bg-gray-50 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full">
+            {datas.flatMap((data) => {
+              if (isThematic(data)) {
+                if (
+                  filter.length < 2 ||
+                  data.name.toUpperCase().includes(filter.toUpperCase())
+                ) {
+                  return (
+                    <li
+                      key={data.id}
+                      className="rounded border border-blue-100 hover:border-blue-600 hover:bg-blue-50 px-8 py-2 text-center"
+                    >
+                      {data.name}
+                    </li>
+                  );
+                }
+              } else if (
+                filter.length < 2 ||
+                data.uname.toUpperCase().includes(filter.toUpperCase())
+              ) {
+                return (
+                  <li
+                    key={data.uid}
+                    className="rounded border border-blue-100 hover:border-blue-600 hover:bg-blue-50 px-8 py-2 text-center"
+                  >
+                    <Link to={`/users/${data.uid}/thematics`}>
+                      {data.uname}
+                    </Link>
+                  </li>
+                );
+              }
+              return [];
+            })}
+          </ul>
+        </section>
+      </div>
     </div>
   );
 }
