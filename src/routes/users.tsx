034bcce10a2baeff55316ac2f146155b2ba2abc9
Tyson Nemeghaire <nemeghaire.tyson@outlook.be>
2023-04-12 11:42:59 +0200

    Formatage du code

--- src/routes/users.tsx
+++ src/routes/users.tsx
@@ -1,7 +1,7 @@
 import Coopernet from "../services/Coopernet";
 
 export async function loader() {
-  return (await Coopernet.getUsers()).filter((user) => user.uid !== "0").sort(
-    (a, b) => +(a.uname.toUpperCase() > b.uname.toUpperCase())
-  );
+  return (await Coopernet.getUsers())
+    .filter((user) => user.uid !== "0")
+    .sort((a, b) => +(a.uname.toUpperCase() > b.uname.toUpperCase()));
 }
