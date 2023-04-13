6f11ebd2a5938a3283346d40492653e9683ac58d
TysonNemeghaire1 <nemeghaire.tyson@outlook.be>
2023-04-13 11:53:06 +0200

    Connexion persistente

--- src/routes/users.tsx
+++ src/routes/users.tsx
@@ -1,6 +1,11 @@
 import Coopernet from "../services/Coopernet";
+import { loginWithLocalStorage } from "../services/login";
+import { redirect } from "react-router-dom";
 
 export async function loader() {
+  if (!Coopernet.user.id && !(await loginWithLocalStorage())) {
+    return redirect("/login");
+  }
   return (await Coopernet.getUsers())
     .filter((user) => user.uid !== "0")
     .sort((a, b) => +(a.uname.toUpperCase() > b.uname.toUpperCase()));
