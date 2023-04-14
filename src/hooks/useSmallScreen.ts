856f0ebc5213217230005513ce035fd7383a2de7
Tyson Nemeghaire <nemeghaire.tyson@outlook.be>
2023-04-14 11:25:42 +0200

    Modification : changement du breakpoint pour le layout

--- src/hooks/useSmallScreen.ts
+++ src/hooks/useSmallScreen.ts
@@ -1,10 +1,18 @@
 import { useLayoutEffect, useState } from "react";
 
+const enum Breakpoints {
+  SM = 640,
+  MD = 768,
+  LG = 1024,
+  XL = 1280,
+  XL2 = 1536,
+}
+
 export function useSmallScreen() {
   const [isSmall, setIsSmall] = useState(false);
   useLayoutEffect(() => {
     function updateSize() {
-      if (window.innerWidth > 1536) setIsSmall(false);
+      if (window.innerWidth > Breakpoints.XL) setIsSmall(false);
       else setIsSmall(true);
     }
 
